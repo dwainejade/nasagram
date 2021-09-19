a1e6e4168b9327cbd39391b05fe0f712bf3899a4
Dwaine Matthew <46469243+dwainejade@users.noreply.github.com>
2021-09-19 16:20:26 -0400

    minor styling

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,36 +1,34 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 // import { PhotoContext } from '../components/PhotoContext'
-import axios from 'axios'
-import Card from '../components/Card'
-import Spinner from '../components/Spinner'
+import axios from "axios";
+import Card from "../components/Card";
+import Spinner from "../components/Spinner";
 
 const Home = () => {
-    const [photos, setPhotos] = useState([])
+  const [photos, setPhotos] = useState([]);
 
+  const fetchPhoto = async () => {
+    const { data } = await axios.get(
+      `https://api.nasa.gov/planetary/apod?api_key=nLkCcfvqTIIGZoD3O0cP2GDGQn5xRkg0CalOXlB8`
+    );
+    setPhotos(data);
+  };
 
-    const fetchPhoto = async () => {
-        const { data } = await axios.get(
-            `https://api.nasa.gov/planetary/apod?api_key=nLkCcfvqTIIGZoD3O0cP2GDGQn5xRkg0CalOXlB8`
-        )
-        setPhotos(data)
-    }
+  useEffect(() => {
+    fetchPhoto();
+  }, []);
 
-    useEffect(() => {
-        fetchPhoto();
-    }, []);
-
-    return (
-        photos.url ?
-            <div>
-                <h1>Home</h1>
-                <Card fav={photos} />
-            </div>
-            :
-            <div>
-                <h1>Home</h1>
-                <Spinner />
-            </div>
-    )
-}
+  return photos.url ? (
+    <div style={{ height: "100vh" }}>
+      <h1>Home</h1>
+      <Card fav={photos} />
+    </div>
+  ) : (
+    <div>
+      <h1>Home</h1>
+      <Spinner />
+    </div>
+  );
+};
 
 export default Home;
--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,54 +1,57 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 // import { PhotoContext } from '../components/PhotoContext'
-import axios from 'axios'
-import CardComponent from '../components/Card'
-import Spinner from '../components/Spinner'
+import axios from "axios";
+import CardComponent from "../components/Card";
+import Spinner from "../components/Spinner";
 
 // import './Card.css'
 
 const Search = () => {
-    // const [favorites, setFavorites, handleFave, getFaves, addFaves] = useContext(PhotoContext)
-    const [photos, setPhotos] = useState([])
-    const [date, setDate] = useState('2020-10-01')
+  // const [favorites, setFavorites, handleFave, getFaves, addFaves] = useContext(PhotoContext)
+  const [photos, setPhotos] = useState([]);
+  const [date, setDate] = useState("2020-10-01");
 
-    const fetchPhoto = async () => {
-        const { data } = await axios.get(
-            `https://api.nasa.gov/planetary/apod?date=${date}&api_key=nLkCcfvqTIIGZoD3O0cP2GDGQn5xRkg0CalOXlB8`
-        )
-        setPhotos(data)
-    }
-
-    useEffect(() => {
-        fetchPhoto();
-    }, [date]);
-
-
-    const handleDate = (e) => {
-        setDate(e.target.value)
-        console.log('date', e.target.value)
-        fetchPhoto()
-    }
-
-
-    return (
-        <>
-            <h1>Search</h1>
-            <div className="date-picker">
-                <input
-                    type="date"
-                    className="form-control"
-                    id="Date"
-                    name="date"
-                    value={date}
-                    onChange={handleDate}
-                />
-            </div>
-            <div className="card-wrapper">
-                <CardComponent fav={photos} />
-            </div>
-        </>
+  const fetchPhoto = async () => {
+    const { data } = await axios.get(
+      `https://api.nasa.gov/planetary/apod?date=${date}&api_key=nLkCcfvqTIIGZoD3O0cP2GDGQn5xRkg0CalOXlB8`
     );
-}
+    setPhotos(data);
+  };
 
+  useEffect(() => {
+    fetchPhoto();
+  }, [date]);
 
-export default Search
+  const handleDate = (e) => {
+    setDate(e.target.value);
+    console.log("date", e.target.value);
+    fetchPhoto();
+  };
+
+  return photos.url ? (
+    <div style={{ height: "100vh" }}>
+      <h1>Search</h1>
+
+      <div className="date-picker" style={{ marginBottom: "1rem" }}>
+        <input
+          type="date"
+          className="form-control"
+          id="Date"
+          name="date"
+          value={date}
+          onChange={handleDate}
+        />
+      </div>
+      <div className="card-wrapper">
+        <CardComponent fav={photos} />
+      </div>
+    </div>
+  ) : (
+    <div>
+      <h1>Search</h1>
+      <Spinner />
+    </div>
+  );
+};
+
+export default Search;
